cda3ecdaeb3b6844b6d575dc998191290f7252e4
Rachel <rlecover@gmail.com>
2016-09-05 09:02:01 -0400

    change fPCA code to not require explictly naming columns

--- optimization/sensitivityanalysis/PCA/doFPCA.jl
+++ optimization/sensitivityanalysis/PCA/doFPCA.jl
@@ -1,3 +1,6 @@
+using RCall;
+using DataFrames;
+
 function generateSampleData(numpatients, numtimepoints)
 	#generate a matrix with numpatients row and numtimepoints colms
 	data = 100.0+25.0*(.5-rand(numpatients,numtimepoints))
@@ -10,13 +13,15 @@ function doPCA(data)
 	N = size(data,2)
 	M = size(data,1)
 	mn = mean(data,2)
-	mnfilled= fill(mn, M, N)
-	data =data-mnfilled #subtract off mean
-	@show typeof(data)
+	data =data.-mn #subtract off mean
 	cov = 1./(N-1)*data*transpose(data)
-	@show typeof(cov)
-	d,v = eig(cov)
-	@show v
+	v,PC = eig(cov)
+	sortedindices =sortperm(-v)
+	#extract the largest ones
+	v = sort(v)
+	PC = PC[:,sortedindices]
+	#project data
+	signals = transpose(PC)*data 
 	
 end
 
@@ -28,7 +33,21 @@ function doPCAoverTime(data)
 end
 
 function main()
-	sampleData = generateSampleData(10,100)
-	@show typeof(sampleData)
-	doPCA(sampleData)
+	sampleData = generateSampleData(3,10)
+	times = float(ones(3,10).*transpose(collect(1:10)))
+	#load the library neccessary into R
+	R"library(fdapace)"
+	@rput (sampleData)
+	@rput times
+	R"sampleData<data.frame(sampleData)"
+	R"times<-data.frame(times)"
+	#convert to lists
+	R"datalist = list()"
+	R"for (i in seq(1,nrow(sampleData))){datalist[[i]]<-as.numeric(as.vector(sampleData[i,]))}"
+	R"timelist = list()"
+	R"for (i in seq(1,nrow(times))){timelist[[i]]<-as.numeric(as.vector(times[i,]))}"
+	
+	outputFPCA=R"FPCA(datalist, timelist, list(error=FALSE, kernel='epan', verbose=TRUE, diagnosticsPlot=TRUE,userBwCov = 10))"
+	convoutput =rcopy(outputFPCA)
+	return convoutput
 end
