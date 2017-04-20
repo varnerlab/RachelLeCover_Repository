2d2ebaee3fb0cdde9292d77191318c50bfbfd6f5
Rachel LeCover <rlecover@purdue.edu>
2017-04-20 16:47:34 -0400

    end of day 4/20. Added code to make complete Sobol graph

--- ReducedOrderModelWPlatelets/AddingFibrin/makeSobolGraph.jl
+++ ReducedOrderModelWPlatelets/AddingFibrin/makeSobolGraph.jl
@@ -2,17 +2,19 @@ using PyPlot
 
 function makeSobolGraph()
 	close("all")
-	fig=figure(figsize=[20,10])
-	data = readdlm("sensitivity/SobolOutput_pm50_tPA2_19_04_2017.txt")
-	topHalf = data[1:47, :]
+	numparams = 77
+	fig=figure(figsize=[25,15])
+	data = readdlm("sensitivity/SobolOutput_pm50_tPA2_N100_04_19_2017.txt")
+	topHalf = data[1:numparams+1, :]
 	@show topHalf
 	usefulData = topHalf[2:end, :]
-	positions = collect(1:46)
-	bar(positions, usefulData[:,4],color = "k", yerr=usefulData[:,5])
+	positions = collect(0:numparams-1)
+	bar(positions, usefulData[:,4],color = "k", yerr=usefulData[:,5], align="center")
 	ax = gca()
 	ax[:xaxis][:set_ticks](positions)
-	ax[:xaxis][:set_ticklabels](usefulData[:,1], rotation = 60, fontsize = 8)
+	ax[:xaxis][:set_ticklabels](usefulData[:,1], rotation = 90, fontsize = 8)
 	ylabel("Total Order Sensitivity Indicies")
-	savefig("sensitivity/SobolTotalOrderN20.pdf")
+	axis([0,numparams,0,1])
+	savefig("sensitivity/SobolTotalOrderN100.pdf")
 	
 end
